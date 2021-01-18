f64389d12bf326c894a8c022d746724dd0fad90b
dartoon <dingxuheng@mail.bnu.edu.cn>
2021-01-18 15:24:11 +0900

    add profile_plots() function

--- decomprofile/tools/plot_tools.py
+++ decomprofile/tools/plot_tools.py
@@ -203,3 +203,124 @@ def total_compare(flux_list_2d, label_list_2d, flux_list_1d, label_list_1d,
     else:
         plt.close()
     return f
+
+
+def profile_plots(flux_list_2d, label_list_2d, flux_list_1d, label_list_1d,
+                  deltaPix = 1., zp=27.0, target_ID = 'target_ID',
+                  mask_image=None, if_annuli=False,
+                  arrows=False, show_plot = True):
+    """
+    The purpose of this def
+    
+    Parameter
+    --------
+        flux_list: a list of 2D flux array:
+            [data, pointsource_list, galaxy_model_list, normalized residual]
+            
+        label_list: a list of lable to show:
+            ['data', 'model', 'point source(s)', 'galaxy(s)']
+        
+    Return
+    --------
+        A sth sth
+    #TODO: test mask 
+    """
+    norm = LogNorm() #ImageNormalize(stretch=SqrtStretch())
+    cl_num = len(flux_list_2d) + 1 
+    f = plt.figure(0, figsize=(6.5+ (cl_num-1)*3.5,4))    
+    # f = plt.figure(0, figsize=(17.0,4))  #3
+    # f = plt.figure(0, figsize=(20.5,4))  #4
+    # f = plt.figure(0, figsize=(24.0,4))  #5
+    ax_l = [plt.subplot2grid((6,cl_num), (0,i), rowspan=6) for i in range(len(flux_list_2d))] #The image plot
+    ax_rt = plt.subplot2grid((6,cl_num), (0,cl_num-1), rowspan=6)
+    # ax_rb = plt.subplot2grid((6,cl_num), (5,cl_num-1), rowspan=1)
+    frame_size = len(flux_list_2d[0])
+    mask = np.ones_like(flux_list_2d[0])
+    if mask_image is not None:
+        mask = mask * mask_image
+    for i in range(len(flux_list_2d)):
+        if i >1:
+            flux_list_2d[i] = flux_list_2d[i] * mask
+        if i == 0:
+            im_i = ax_l[i].imshow(flux_list_2d[i] * mask ,origin='lower',cmap=my_cmap, norm=norm,
+                                  vmax = flux_list_2d[0].max(), vmin = 1.e-4)
+            clim=im_i.properties()['clim'] #To uniform the color bar scale.
+            ax_l[i].set_ylabel(target_ID, fontsize=15, weight='bold')
+        else:
+            im_i = ax_l[i].imshow(flux_list_2d[i],origin='lower',cmap=my_cmap, norm=norm, clim=clim)
+            ax_l[i].get_yaxis().set_visible(False)
+        ax_l[i].get_xaxis().set_visible(False)
+        scale_bar(ax_l[i], frame_size, dist=1/deltaPix, text='1"', color = 'white')
+        if arrows == True:
+            coordinate_arrows(ax_l[i], frame_size, arrow_size=0.03, color = 'white')
+        cb_i = f.colorbar(im_i, ax=ax_l[i], shrink=0.48, pad=0.01,  orientation="horizontal", aspect=15, ticks= [1.e-4, 1.e-3, 1.e-2,1.e-1,0, 10])
+        cb_i.set_ticks([1.e-5, 1.e-4, 1.e-3, 1.e-2,1.e-1,0,1,10,100])   
+        if len(label_list_2d[i])>10:
+            fontsize = 17
+        else:
+            fontsize = 20
+        ax_l[i].text(frame_size*0.05, frame_size*0.9, label_list_2d[i],fontsize=fontsize, weight='bold', color='white')
+
+    plt.subplots_adjust(wspace=-0.45, hspace=0)
+        
+    #Plot the 1D profile:
+    label_SB_list = label_list_1d #Not show the residual, in order of data, model, QSO, galaxy in principle.
+    flux_SB_list = flux_list_1d
+    radi = len(flux_list_1d[0])/2
+    if if_annuli == False:
+        for i in range(len(label_SB_list)):
+            center = len(flux_SB_list[i])/2, len(flux_SB_list[i])/2
+            if label_SB_list[i] == 'data':
+                r_SB, r_grids = SB_profile(flux_SB_list[i], center, x_gridspace = 'log',
+                                           radius= radi, grids = 50,
+                                           mask_image=mask_image, fits_plot=False)
+            else:
+                r_SB, r_grids = SB_profile(flux_SB_list[i], center, x_gridspace = 'log', radius= radi,
+                                           grids = 30, mask_image = mask_image)
+            r_mag = - 2.5 * np.log10(r_SB) + zp 
+            if label_SB_list[i] == 'data':
+                ind = len(r_mag)-(r_mag == r_mag[-1]).sum()
+                ax_rt.plot(r_grids[:ind], r_mag[:ind], 'o', color = 'whitesmoke',markeredgecolor="black", label=label_SB_list[i])
+            else:
+                ax_rt.plot(r_grids, r_mag, '-', label=label_SB_list[i])
+        ax_rt.set_ylabel('$\mu$(mag, pixel$^{-2}$)', fontsize=12)
+        ax_rt.invert_yaxis()
+    elif if_annuli == True:
+        for i in range(len(label_SB_list)):
+            center = len(flux_SB_list[i])/2, len(flux_SB_list[i])/2
+            if label_SB_list[i] == 'data':
+                r_SB, r_grids = SB_profile(flux_SB_list[i], center, x_gridspace = 'log',
+                                           radius = radi, grids = 50, 
+                                           mask_image = mask_image, fits_plot=False, if_annuli = if_annuli)
+                ax_rt.plot(r_grids, r_SB, 'o', color = 'whitesmoke',markeredgecolor="black", label=label_SB_list[i])
+            else:
+                r_SB, r_grids = SB_profile(flux_SB_list[i], center, x_gridspace = 'log',
+                                           radius=radi,grids = 30, mask_image=mask_image, if_annuli = if_annuli)
+                ax_rt.plot(r_grids, r_SB, '-', label=label_SB_list[i])
+        ax_rt.set_ylabel('$SB_{annuli}$(counts, pixel$^{-2}$)', fontsize=12)
+    ax_rt.set_xlabel('pixel', fontsize=15)
+    
+    ax_rt.set_xscale('log')
+    ax_rt.set_xticks([2,4,6,10,15,20,30,50,100,150])
+    ax_rt.xaxis.set_major_formatter(ScalarFormatter())
+    ax_rt.set_xlim([(r_grids).min()*0.85,r_grids.max()+6])
+    
+    #TODO!!!: Update the axis in arcsec on upside: 
+    # ax_rt2 = ax_rt.twiny()
+    # new_tick_locations = np.array([ax_rt.get_xlim()[0], 0.2, 0.5, 1, 2,5 , ax_rt.get_xlim()[1]])*deltaPix
+    # ax_rt2.set_xticks(new_tick_locations)
+    # ax_rt2.set_xticklabels([0.1, 0.2, 0.5, 1, 2,5])
+    # ax_rt2.set_xlabel("arcsec")    
+    
+    ax_rt.yaxis.set_label_position('right')
+    ax_rt.yaxis.tick_right()
+    ax_rt.yaxis.set_ticks_position('both') 
+    ax_rt.legend()
+    pos4_o = ax_rt.get_position() # get the original position
+    pos4 = [pos4_o.x0+0.112 - 0.009 * cl_num , pos4_o.y0 + 0.10, pos4_o.width*0.72, pos4_o.height*0.8]
+    ax_rt.set_position(pos4) # set a new position
+    if show_plot == True:
+        plt.show()       
+    else:
+        plt.close()
+    return f
