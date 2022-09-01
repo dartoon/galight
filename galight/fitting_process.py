3d5b475b7c35caf3fc9c33be20617ec906236611
dartoon <dingxuheng@mail.bnu.edu.cn>
2022-09-01 15:54:39 +0900

    refresh fun; 'linear_solver' off fun; update dump

--- galight/fitting_process.py
+++ galight/fitting_process.py
@@ -36,7 +36,7 @@ class FittingProcess(object):
                 - 'deep', perfer a deep fitting, with more fitting PSO and/or MCMC particles, which takes more time.
                 - 'shallow', perfer a quick fitting.
     """
-    def __init__(self, fitting_specify_class, savename = 'result', fitting_level='norm'):
+    def __init__(self, fitting_specify_class, savename = 'result', fitting_level=None):
         fitting_specify_class.build_fitting_seq()
         self.fitting_specify_class = fitting_specify_class
         self.fitting_seq = fitting_specify_class.fitting_seq
@@ -73,33 +73,47 @@ class FittingProcess(object):
             fitting_kwargs_list.append([algorithm_list[i], setting])
         self.fitting_kwargs_list = fitting_kwargs_list
     
-    def run(self, algorithm_list = ['PSO', 'PSO'], setting_list = None, threadCount=1):
+    def run(self, algorithm_list = ['PSO', 'PSO'], fitting_level = 'norm', 
+            setting_list = None, threadCount=1, refresh = False):
         """
         Run the fitting. The algorithm_list and setting_list will be pass to 'fitting_kwargs()'
         """
         if setting_list is None:
             setting_list = [None] * len(algorithm_list)
+        if self.fitting_level is None:
+            self.fitting_level = fitting_level
         self.fitting_kwargs(algorithm_list = algorithm_list, setting_list = setting_list, threadCount=threadCount)
         fitting_specify_class = self.fitting_specify_class
-        start_time = time.time()
-        chain_list = self.fitting_seq.fit_sequence(self.fitting_kwargs_list)
-        kwargs_result = self.fitting_seq.best_fit()
+        if refresh == False:
+            start_time = time.time()
+            chain_list = self.fitting_seq.fit_sequence(self.fitting_kwargs_list)
+            kwargs_result = self.fitting_seq.best_fit()
+            if self.fitting_kwargs_list[-1][0] == 'MCMC':
+                self.sampler_type, self.samples_mcmc, self.param_mcmc, self.dist_mcmc  = chain_list[-1]    
+            end_time = time.time()
+            print(round(end_time - start_time, 3), 'total time taken for the overall fitting (s)')
+            print('============ CONGRATULATION, YOUR JOB WAS SUCCESSFUL ================ ')
+        elif refresh == True:
+            kwargs_result = self.kwargs_result
+            chain_list = self.chain_list
         ps_result = kwargs_result['kwargs_ps']
         source_result = kwargs_result['kwargs_lens_light']
-        if self.fitting_kwargs_list[-1][0] == 'MCMC':
-            self.sampler_type, self.samples_mcmc, self.param_mcmc, self.dist_mcmc  = chain_list[-1]    
-        end_time = time.time()
-        print(round(end_time - start_time, 3), 'total time taken for the overall fitting (s)')
-        print('============ CONGRATULATION, YOUR JOB WAS SUCCESSFUL ================ ')
-        
-        from lenstronomy.ImSim.image_linear_solve import ImageLinearFit
-        imageLinearFit = ImageLinearFit(data_class=fitting_specify_class.data_class, 
-                                        psf_class=fitting_specify_class.psf_class,
-                                        lens_light_model_class=fitting_specify_class.lightModel,
-                                        point_source_class=fitting_specify_class.pointSource, 
-                                        kwargs_numerics=fitting_specify_class.kwargs_numerics)    
-        image_reconstructed, error_map, _, _ = imageLinearFit.image_linear_solve(kwargs_lens_light=source_result,
-                                                                                 kwargs_ps=ps_result)
+            
+        if 'linear_solver' in fitting_specify_class.kwargs_constraints.keys():
+            linear_solver = fitting_specify_class.kwargs_constraints['linear_solver']
+        else:
+            linear_solver = True
+        if linear_solver == True:
+            from lenstronomy.ImSim.image_linear_solve import ImageLinearFit
+            imageLinearFit = ImageLinearFit(data_class=fitting_specify_class.data_class, 
+                                            psf_class=fitting_specify_class.psf_class,
+                                            lens_light_model_class=fitting_specify_class.lightModel,
+                                            point_source_class=fitting_specify_class.pointSource, 
+                                            kwargs_numerics=fitting_specify_class.kwargs_numerics)    
+            image_reconstructed, error_map, _, _ = imageLinearFit.image_linear_solve(kwargs_lens_light=source_result,
+                                                                                      kwargs_ps=ps_result)
+            self.imageLinearFit = imageLinearFit
+            self.reduced_Chisq_bylenstronomy =  imageLinearFit.reduced_chi2(image_reconstructed, error_map)
         imageModel = fitting_specify_class.imageModel
         image_host_list = []  #The linear_solver before and after LensModelPlot could have different result for very faint sources.
         for i in range(len(source_result)):
@@ -139,8 +153,9 @@ class FittingProcess(object):
                 kwargs_out = param.args2kwargs(self.samples_mcmc[i])
                 kwargs_light_source_out = kwargs_out['kwargs_lens_light']
                 kwargs_ps_out =  kwargs_out['kwargs_ps']
-                image_reconstructed, _, _, _ = imageLinearFit.image_linear_solve(kwargs_lens_light=kwargs_light_source_out,
-                                                                                      kwargs_ps=kwargs_ps_out)
+                if linear_solver == True:
+                    image_reconstructed, _, _, _ = imageLinearFit.image_linear_solve(kwargs_lens_light=kwargs_light_source_out,
+                                                                                          kwargs_ps=kwargs_ps_out)
                 flux_list_quasar = []
                 if len(fitting_specify_class.point_source_list) > 0:
                     for j in range(len(fitting_specify_class.point_source_list)):
@@ -173,12 +188,11 @@ class FittingProcess(object):
         self.kwargs_result = kwargs_result
         self.ps_result = ps_result
         self.source_result = source_result
-        self.imageLinearFit = imageLinearFit
         self.image_host_list = image_host_list
         self.image_ps_list = image_ps_list
         self.translate_result()
-        self.reduced_Chisq_bylenstronomy =  imageLinearFit.reduced_chi2(image_reconstructed, error_map)
         self.reduced_Chisq, self.reduced_Chisq_dof = self.cal_chisq() 
+        self.linear_solver = linear_solver
 
     def run_diag(self, diag_list = None, show_plot = True):
         """
@@ -210,13 +224,13 @@ class FittingProcess(object):
             modelPlot = ModelPlot(self.fitting_specify_class.kwargs_data_joint['multi_band_list'],
                                   self.fitting_specify_class.kwargs_model, self.kwargs_result,
                                   arrow_size=0.02, cmap_string="gist_heat", 
-                                  image_likelihood_mask_list=self.fitting_specify_class.kwargs_likelihood['image_likelihood_mask_list'] )    
+                                  image_likelihood_mask_list=self.fitting_specify_class.kwargs_likelihood['image_likelihood_mask_list'],
+                                  linear_solver = self.linear_solver)    
         except:
-            print("Please upgrade your lenstronomy version!")
             modelPlot = ModelPlot(self.fitting_specify_class.kwargs_data_joint['multi_band_list'],
                                   self.fitting_specify_class.kwargs_model, self.kwargs_result,
                                   arrow_size=0.02, cmap_string="gist_heat", 
-                                  likelihood_mask_list=self.fitting_specify_class.kwargs_likelihood['image_likelihood_mask_list'] )    
+                                  image_likelihood_mask_list=self.fitting_specify_class.kwargs_likelihood['image_likelihood_mask_list'])    
             
         f, axes = plt.subplots(3, 3, figsize=(16, 16), sharex=False, sharey=False)
         modelPlot.data_plot(ax=axes[0,0], text="Data")
@@ -276,11 +290,11 @@ class FittingProcess(object):
         """ 
         data = self.fitting_specify_class.kwargs_data['image_data']
         if 'psf_error_map' in self.fitting_specify_class.kwargs_psf.keys():
-            modelPlot = ModelPlot(self.fitting_specify_class.kwargs_data_joint['multi_band_list'],
+            _modelPlot = ModelPlot(self.fitting_specify_class.kwargs_data_joint['multi_band_list'],
                                   self.fitting_specify_class.kwargs_model, self.kwargs_result,
                                   arrow_size=0.02, cmap_string="gist_heat", 
-                                  likelihood_mask_list=self.fitting_specify_class.kwargs_likelihood['image_likelihood_mask_list'] )    
-            _, psf_error_map, _, _ = modelPlot._imageModel.image_linear_solve(inv_bool=True, **self.kwargs_result)
+                                  image_likelihood_mask_list=self.fitting_specify_class.kwargs_likelihood['image_likelihood_mask_list'] )    
+            _, psf_error_map, _, _ = _modelPlot._imageModel.image_linear_solve(inv_bool=True, **self.kwargs_result)
             noise = np.sqrt(self.fitting_specify_class.kwargs_data['noise_map']**2+np.abs(psf_error_map[0]))
         else:
             noise = self.fitting_specify_class.kwargs_data['noise_map']
@@ -539,7 +553,13 @@ class FittingProcess(object):
         savename = self.savename
         dump_class = copy.deepcopy(self)
         if hasattr(dump_class.fitting_specify_class, 'data_process_class') and savedata==False:
-            del dump_class.fitting_specify_class.data_process_class
+            if hasattr(dump_class.fitting_specify_class.data_process_class, 'fov_image'):
+                del dump_class.fitting_specify_class.data_process_class.fov_image
+            if hasattr(dump_class.fitting_specify_class.data_process_class, 'fov_noise_map'):
+                del dump_class.fitting_specify_class.data_process_class.fov_noise_map
+            if hasattr(dump_class.fitting_specify_class.data_process_class, 'exptime'):
+                if isinstance(dump_class.fitting_specify_class.data_process_class.exptime, np.ndarray):
+                    del dump_class.fitting_specify_class.data_process_class.exptime
         if dump_class.fitting_specify_class.kwargs_likelihood['custom_logL_addition'] != None:
             dump_class.prior = str(dump_class.fitting_specify_class.kwargs_likelihood['custom_logL_addition'])
             del dump_class.fitting_specify_class.kwargs_likelihood['custom_logL_addition']
