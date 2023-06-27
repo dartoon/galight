d7323416a4d6cbb4a509069dde5198b5dc0bc745
dartoon <dingxuheng@mail.bnu.edu.cn>
2023-06-27 10:10:22 +0900

    Update version

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
     name='galight',
-    version='0.1.12',
+    version='0.1.13',
     description='Galaxy light 2D modeling tool',
     long_description=readme,
     long_description_content_type='text/markdown',
